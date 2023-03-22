d1df50046bcd5853ce829e2106419a6017633f87
AlexandraHaim <alexandra.haim@hotmail.fr>
2023-03-22 09:16:43 +0100

    renommage des photos

---
+++ rename.py
@@ -0,0 +1,29 @@
+# Script permettant de renommer les fichiers des répertoires : 
+#  /database/Apprentissage/ : AsieN_A_<compteur>.png
+#  /database/Validation/ : AsieN_V_<compteur>.png
+#  /database/Test/ : AsieN_T_<compteur>.png
+
+#!/usr/bin/env python3
+
+import os
+import glob
+
+# Renommer les fichiers
+def rename_files(path, suffixe):
+    # Récupérer les fichiers
+    files = glob.glob(path + "/*")
+    print(len(files) + " fichiers trouvés.")
+    # Parcourir les fichiers
+    i = 0
+    for file in files:
+        # Renommer le fichier
+        os.rename(file, "<region>" + suffixe + "_" + str(i) + ".png")
+        i += 1
+    print("Vos fichiers sont sûrement au même niveau que rename.py")
+
+# Renommer les fichiers du répertoire Apprentissage
+rename_files("<mettre votre path>/ApprentissageProfond/DataBase/Apprentissage/<region>/", "A")
+# Renommer les fichiers du répertoire Validation
+rename_files("<mettre votre path>/ApprentissageProfond/DataBase/Validation/<region>/", "V")
+# Renommer les fichiers du répertoire Test
+rename_files("<mettre votre path>/ApprentissageProfond/DataBase/Test/<region>/", "T")
